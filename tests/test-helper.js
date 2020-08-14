ac178051905a7c64482a4d62c50d70774ee5cd71
Erika Pauwels <erika.pauwels@gmail.com>
2020-08-14 16:44:17 +0200

    Upgrade Ember v3.17.0...v3.20.0

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,5 +1,5 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'frontend-price-management/app';
+import config from 'frontend-price-management/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
