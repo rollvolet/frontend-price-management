ba75da213fe4d93d3a8b5f57a6b11b3ad33c8d81
Erika Pauwels <erika.pauwels@gmail.com>
2020-08-14 18:44:42 +0200

    Add TailwindUI

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,13 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    postcssOptions: {
+      compile: {
+        plugins: [
+          require('tailwindcss')('./app/styles/tailwind.config.js')
+        ]
+      }
+    }
     // Add options here
   });
 
