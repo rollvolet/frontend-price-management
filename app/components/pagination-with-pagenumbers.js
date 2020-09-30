3b04e790389d3f8c1e369a6a3ec924a5e851d7b3
Erika Pauwels <erika.pauwels@gmail.com>
2020-09-30 22:28:44 +0200

    Fix off-by-one error and number sorting in pagination component

--- app/components/pagination-with-pagenumbers.js
+++ app/components/pagination-with-pagenumbers.js
@@ -33,7 +33,7 @@ export default class PaginationWithPagenumbersComponent extends Component {
   }
 
   get lastPage() {
-    return Math.floor(this.args.total / this.size); // page is zero-based
+    return Math.floor((this.args.total - 1)/ this.size); // page is zero-based
   }
 
   get nbOfPages() {
@@ -56,7 +56,7 @@ export default class PaginationWithPagenumbersComponent extends Component {
         const leftWindow = [x, x + 1, x + 2].filter(i => i <= this.lastPage);
         const y = this.lastPage;
         const rightWindow = [y - 2, y - 1, y].filter(i => i >= this.firstPage);
-        const pages = [...leftWindow, ...rightWindow].uniq().sort();
+        const pages = [...leftWindow, ...rightWindow].uniq().sort(function(a, b) { return a - b; });
         if (pages.length == 6 && pages[2] < pages[3] - 1) {
           return [...leftWindow, more, ...rightWindow];
         } else {
