937e41d0509d1837f8778a33e8a0fa02d39857ef
Erika Pauwels <erika.pauwels@gmail.com>
2020-11-02 11:48:16 +0100

    Add product edit route

--- app/routes/main/products/edit.js
+++ app/routes/main/products/edit.js
@@ -1,4 +1,24 @@
 import Route from '@ember/routing/route';
 
 export default class MainProductsEditRoute extends Route {
+  async model(params) {
+    const products = await this.store.query('product', {
+      filter: { ':id:': params.product_id },
+      page: { size: 1 }
+    });
+    return products.firstObject;
+  }
+
+  async afterModel(model) {
+    const category = await model.category;
+    if (category)
+      this.broaderCategory = await category.broader;
+    else
+      this.broaderCategory = null;
+  }
+
+  setupController(controller) {
+    super.setupController(...arguments);
+    controller.broaderCategory = this.broaderCategory;
+  }
 }
