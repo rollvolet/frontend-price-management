6fef7ccfecd2617366a355ae997a9301bac0567f
Erika Pauwels <erika.pauwels@gmail.com>
2020-12-19 10:50:38 +0100

    Remove margin and add price tax incl. and excl. in products table

--- app/components/fmt/unit-price.js
+++ app/components/fmt/unit-price.js
@@ -3,10 +3,24 @@ import Component from '@glimmer/component';
 const VAT_RATE = 0.21;
 
 export default class FmtUnitPriceComponent extends Component {
+  get currencyValue() {
+    if (this.args.showTaxIncluded)
+      return this.currencyValueTaxIncluded;
+    else
+      return this.currencyValueTaxExcluded;
+  }
+
   get currencyValueTaxIncluded() {
     if (this.args.model.valueAddedTaxIncluded)
       return this.args.model.currencyValue;
     else
       return this.args.model.currencyValue * (1 + VAT_RATE);
+  }
+
+  get currencyValueTaxExcluded() {
+    if (this.args.model.valueAddedTaxIncluded)
+      return this.args.model.currencyValue / (1 + VAT_RATE);
+    else
+      return this.args.model.currencyValue;
   }
 }
