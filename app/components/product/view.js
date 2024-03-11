5ab1ae1a56d13b2bd866cafe2768363ed083e8bc
Michaël Dierick <michael.dierick@redpencil.io>
2024-03-11 16:12:52 +0100

    add view route to provide languages constant

---
+++ app/components/product/view.js
@@ -0,0 +1,6 @@
+import Component from '@glimmer/component';
+import { SUPPORTED_LANGUAGES } from '../../config';
+
+export default class ProductViewComponent extends Component {
+  SUPPORTED_LANGUAGES = SUPPORTED_LANGUAGES;
+}
