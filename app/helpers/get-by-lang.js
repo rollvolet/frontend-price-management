830b33c80a95bcd09b4c59aa6a3dfc84c97d295d
Erika Pauwels <erika.pauwels@gmail.com>
2024-03-22 10:19:19 +0100

    Remove assertions in get-by-lang helper

--- app/helpers/get-by-lang.js
+++ app/helpers/get-by-lang.js
@@ -4,17 +4,8 @@ export default function getByLang(object, lang) {
   lang = lang || DEFAULT_LANGUAGE;
   if (Array.isArray(object)) {
     const objectsByLang = object.filter((o) => o.language === lang);
-    if (object.length > 0) {
-      console.assert(
-        objectsByLang.length > 0,
-        `requested language '${lang}' not available from ${object}`
-      );
-    }
     return objectsByLang.map((o) => o.content);
+  } else {
+    return object.content;
   }
-  console.assert(
-    lang === object.language,
-    `requested language ${lang} not available from ${object}`
-  );
-  return object.content;
 }
