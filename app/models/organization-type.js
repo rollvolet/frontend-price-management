20cae81bb5efc20e2bf64f0c9ea2775cb1e04d18
Erika Pauwels <erika.pauwels@gmail.com>
2024-09-24 10:48:06 +0200

    Remove unused models

--- app/models/organization-type.js
+++
@@ -1,7 +0,0 @@
-import Model, { attr } from '@ember-data/model';
-
-export default class OrganizationTypeModel extends Model {
-  @attr('string') label;
-  @attr('string') notation;
-  @attr('string') conceptScheme;
-}
