41a349e2523ffb9abb42122b1d4788e4e166d1c3
Ruben <ruben.de.facq@redpencil.io>
2024-10-30 14:18:40 +0100

    fix deprecation (inverse missing)

--- app/models/data-container.js
+++ app/models/data-container.js
@@ -3,5 +3,5 @@ import Model, { attr, hasMany } from '@ember-data/model';
 export default class DataContainerModel extends Model {
   @attr('string') uri;
   @attr('string-set') content;
-  @hasMany('file', { async: true }) files;
+  @hasMany('file', { async: true, inverse: null }) files;
 }
