cc1eb7a981cafc3c0dec3a6d8a85b39afcd88041
Ruben <ruben.de.facq@redpencil.io>
2024-11-13 21:53:05 +0100

    only show preview button if image attachment exists
    to avoid loading all attachments of every product, resources is used to filter on the attachment format.

--- app/controllers/main/products/list/index.js
+++ app/controllers/main/products/list/index.js
@@ -1,10 +1,12 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { trackedFunction } from 'reactiveweb/function';
+import { service } from '@ember/service';
 export default class MainProductsListIndexController extends Controller {
-
   @tracked previewProduct;
-  
+  @service store;
+
   @action
   showPreview(product) {
     this.previewProduct = product;
@@ -14,4 +16,18 @@ export default class MainProductsListIndexController extends Controller {
   closePreview() {
     this.previewProduct = undefined;
   }
+
+  hasPreview = (product) => {
+    return this.productIdsWithImages.value?.some(pId => pId === product.id);
+  }
+
+  productIdsWithImages = trackedFunction(this, async () => {
+    const searchProductIds = this.model.map((p) => p.id);
+    console.log(searchProductIds);
+    const productsWithImage = await this.store.query('product', {
+      'filter[:id:]': searchProductIds.join(','),
+      'filter[attachments][format]': 'image/',
+    });
+    return productsWithImage.map(p => p.id);
+  });
 }
