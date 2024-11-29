551d21494bedc1c6d2642da8ee302139a4c6ee51
Erika Pauwels <erika.pauwels@gmail.com>
2024-11-29 13:18:40 +0100

    Update layout of export trigger and download buttons

--- app/controllers/main/products.js
+++ app/controllers/main/products.js
@@ -1,5 +1,5 @@
 import Controller from '@ember/controller';
-import { task, timeout } from 'ember-concurrency';
+import { task } from 'ember-concurrency';
 import { service } from '@ember/service';
 import { action } from '@ember/object';
 import TaskModel from 'frontend-price-management/models/task';
@@ -13,7 +13,8 @@ export default class MainProductsController extends Controller {
     return this.router.currentRouteName == 'main.products.index';
   }
 
-  async createExportTask(operation) {
+  @task
+  *createExport(operation) {
     const now = new Date();
     const exportTask = this.store.createRecord('task', {
       operation: operation,
@@ -21,19 +22,22 @@ export default class MainProductsController extends Controller {
       modified: now,
       status: TaskModel.SCHEDULED,
     });
-    return exportTask.save();
+    yield exportTask.save();
+
+    yield this.taskMonitor.monitor.perform(exportTask);
+    if (exportTask.isSuccessful) {
+      const resultContainer = yield exportTask.resultContainer;
+      const files = yield resultContainer.files;
+      const file = files[0];
+      return file;
+    } else {
+      throw 'Export task failed';
+    }
   }
 
-  @task
-  *createExport(operation) {
-    const task = yield this.createExportTask(operation);
-    yield this.taskMonitor.monitor.perform(task);
-    if (!task.isSuccessful) {
-      throw 'Exporttask failed executing';
-    }
-    const resultContainer = yield task.resultContainer;
-    const files = yield resultContainer.files;
-    const file = files[0];
-    return file;
+  @action
+  openExportFile() {
+    window.open(this.createExport.lastSuccessful.value.downloadLink, '_blank');
+    this.createExport.cancelAll({ resetState: true });
   }
 }
